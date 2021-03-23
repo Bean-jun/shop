dea00a6a01a84c7d8ce5917263306a1581c894e7
Bean-jun <1342104001@qq.com>
2021-03-23 13:55:34 +0800

    # fix(storage): 修复文件存储类的部分上传bug

--- utils/fdfs/storage.py
+++ utils/fdfs/storage.py
@@ -1,5 +1,7 @@
+import os
 from django.core.files.storage import Storage
-from fdfs_client.client import Fdfs_client
+from fdfs_client.client import Fdfs_client, get_tracker_conf
+from django.conf import settings
 
 
 class FDFSDstorage(Storage):
@@ -14,9 +16,12 @@ class FDFSDstorage(Storage):
         # 保存文件时使用
         # name：选择上传文件的名称
         # content: 包含上传文件内容的File类的对象
-    
+
+        # 获取配置文件
+        path = get_tracker_conf(settings.BASE_DIR + '/utils/fdfs/client.conf')
+        
         # 创建一个Fdfs_client对象
-        client = Fdfs_client('./utils/fdfs/cloent.conf')
+        client = Fdfs_client(path)
 
         # 上传文件到fastdfs系统中
         res = client.upload_by_buffer(content.read())
@@ -29,8 +34,8 @@ class FDFSDstorage(Storage):
             # 获取返回文件ID
             filename = res.get('Remote file_id')
         
-        # 返回保存文件，这里返回的是什么，数据表里面就会保存什么内容
-        return filename
+        # 返回保存文件，这里返回的是什么，数据表里面就会保存什么内容,注意返回类型为bytes类型
+        return filename.decode()
 
     def exists(self, name):
         # Django判断文件名是否可用
