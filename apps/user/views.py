f4b2a97ad50bf09c7b398002c4ceb987664abc8a
Bean-jun <1342104001@qq.com>
2021-03-24 13:17:32 +0800

    # feat: 调整用户中心的信息页、订单页、地址页      1、新增用户中心非登录用户的限制      2、添加用户中心三个页面
    # 声明：由于目前正在学习前端内容，故在此期间使用之前的模板HTML文件，后期打算替换，故此目前不考虑模板继承

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,13 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views.generic import View
-from apps.user.models import User
+from apps.user.models import User, Address
 from django.urls import reverse
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from itsdangerous import SignatureExpired
 from django.conf import settings
 from django.core.mail import send_mail
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin   # 对未登录账户限制访问
+
+
+"""
+注册
+    注册时校验用户是否已经被注册 √
+    完成用户的注册 √
+    在注册时，需要给用户发送激活链接，让用户点击链接从而完成账户激活 √
+登录
+    实现用户的登录及退出 √
+用户中心
+    用户信息页：显示用户的信息，包括用户名、电话和收货地址 √  用户最近浏览的商品记录
+    用户地址页：显示用户的默认收货地址，页面下方同时可以增加用户的收货地址 √
+    用户订单页：显示用户的订单信息
+其他
+    待补充
+"""
 
 
 class RegisterView(View):
@@ -132,19 +149,27 @@ class LoginView(View):
         # 登录处理
         user = authenticate(request, username=username, password=password)
 
-        response = redirect(reverse('goods:index'))
-
         if user is not None:
-            # 设置cookie
-            if allow == 'on':
-                response.set_cookie('username', username, expires=7*60*60)
+            if user.is_active == 1:
+
+                # 登录
+                login(request, user)
+
+                # 获取未登录账户登录后的跳转页
+                next_url = request.GET.get('next', reverse('goods:index'))
+
+                response = redirect(next_url)
+
+                # 设置cookie
+                if allow == 'on':
+                    response.set_cookie('username', username, expires=7*60*60)
+                else:
+                    response.delete_cookie('username')
+
+                # 返回应答
+                return response
             else:
-                response.delete_cookie('username')
-
-            login(request, user)
-
-            # 返回应答
-            return response
+                return render(request, 'user/register.html', {'error': '请联系管理员激活账号'})
         else:
             # 数据不正确
             return redirect(reverse('user:register'))
@@ -157,3 +182,100 @@ class LogoutView(View):
     def get(self, request):
         logout(request)
         return redirect(reverse('goods:index'))
+
+
+class UserInfoView(LoginRequiredMixin, View):
+    """
+    用户信息页
+    """
+    def get(self, request):
+        # 个人信息获取
+        user = request.user
+        try:
+            address = Address.objects.get(user=user, is_default=True)
+        except Address.DoesNotExist:
+            address = None
+
+        # 浏览记录获取
+
+        # 组织内容
+        context = {
+            'address': address,
+            'active': True,
+        }
+
+        return render(request, 'user/user_center_info.html', context)
+
+
+class UserSiteView(LoginRequiredMixin, View):
+    """
+    用户地址页
+    """
+    def get(self, request):
+
+        # 获取用户收货地址
+        user = request.user
+        try:
+            address = Address.objects.filter(user=user)
+        except Address.DoesNotExist:
+            address = None
+
+        context = {
+            'address': address,
+            'active': True,
+        }
+        return render(request, 'user/user_center_site.html', context)
+
+    def post(self, request):
+        # 获取数据
+        res = request.POST
+        user = request.user
+        receiver = res.get('receiver')
+        addr = res.get('addr')
+        zip_code = res.get('zip_code')
+        phone = res.get('phone')
+        is_default = False
+
+        # 校验数据
+        if not all([receiver, addr, phone]):
+            return render(request, 'user/user_center_site.html', {'error': "数据不完整"})
+
+        # 业务处理
+        # 避免重复添加
+        try:
+            Address.objects.get(user=user,
+                                receiver=receiver,
+                                phone=phone,
+                                addr=addr)
+        except Address.DoesNotExist:
+            # 不存在就添加地址
+            address = Address.objects.create(user=user,
+                                             receiver=receiver,
+                                             phone=phone,
+                                             addr=addr,
+                                             zip_code=zip_code)
+
+            # 查询是否存在默认地址
+            try:
+                Address.objects.filter(user=user).filter(is_default=True)
+            except Address.DoesNotExist:
+                # 不存在默认收货地址
+                is_default = True
+
+            address.is_default = is_default
+            address.save()
+
+        # 返回应答
+        return redirect(reverse('user:site'))
+
+
+class UserOrderView(LoginRequiredMixin, View):
+    """
+    用户订单页
+    """
+    def get(self, request):
+        # 组织内容
+        context = {
+            'active': True,
+        }
+        return render(request, 'user/user_center_order.html', context)
